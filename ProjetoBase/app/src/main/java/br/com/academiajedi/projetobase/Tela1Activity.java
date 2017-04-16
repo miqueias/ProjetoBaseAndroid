d3546963317decde3b08020b3ac7fd15a23298ae
Miqueias Lopes <miqueias.lopes@gmail.com>
2017-04-16 14:55:46 -0300

    implementação da toolbar no projeto;

--- ProjetoBase/app/src/main/java/br/com/academiajedi/projetobase/Tela1Activity.java
+++ ProjetoBase/app/src/main/java/br/com/academiajedi/projetobase/Tela1Activity.java
@@ -1,10 +1,15 @@
 package br.com.academiajedi.projetobase;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +24,10 @@ public class Tela1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setamos o layout que vai ser carregado por essa activity
         setContentView(R.layout.activity_tela1);
+
+        //toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         //importando a fonte
         final Typeface oxygenRegular = Typeface.createFromAsset(getResources().getAssets(), "PressStart2P-Regular.ttf");
@@ -57,7 +66,53 @@ public class Tela1Activity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Toolbar tb = (Toolbar) findViewById(R.id.my_toolbar);
+        tb.inflateMenu(R.menu.menu);
+        tb.setOnMenuItemClickListener(
+                new Toolbar.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        return onOptionsItemSelected(item);
+                    }
+                });
 
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                final AlertDialog.Builder builder = new AlertDialog.Builder(Tela1Activity.this);
+                builder.setMessage("Configuraç˜es")
+                        .setPositiveButton(R.string.app_name, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        })
+                        .setNegativeButton(R.string.app_name, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
